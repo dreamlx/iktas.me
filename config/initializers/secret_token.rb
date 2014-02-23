ef6a49c6c15fad2803561111fd239ff71f956f67
dreamlx <dreamlinx@gmail.com>
2014-02-24 02:07:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IktasMe::Application.config.secret_token = 'ea27ef245c1422aefdfae5f8eb8e08ad55fda2c39b131b65e8eab7cbbd239337ada3ad750836a099c0beb71b3817ea22aa983fcea3b70cefb31cab1f022e8fbb'

738786658332a3ef20dd9704978f5a3283d80610
dreamlx <dreamlinx@gmail.com>
2014-03-01 11:05:56 +0800

    fix style

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,97 +1,57 @@
-set :application, 'iktas'
-set :url, 'http://iktas.me/'
+require 'rvm/capistrano' # 支持rvm
+require 'bundler/capistrano'  # 支持自动bundler
+set :rvm_autolibs_flag, "read-only"        # more info: rvm help autolibs
 
-# SCM
-set :scm, :git
-set :repository, 'git@github.com:dreamlx/iktas_me.git'
-set :branch, 'master'
+set :application, "iktas" #应用的名字
+set :keep_releases, 10 
+set :location, "106.187.45.40" #部署的ip地址
+# set :location, "http://tangdigital.com/"
+role :web, location                       # Your HTTP server, Apache/etc
+role :app, location                       # This may be the same as your `Web` server
+role :db,  location, :primary => true # This is where Rails migrations will run
+#role :db,  "3dtzk.com"
 
-# Server
-server '106.187.45.40', :app, :web, :db, primary: true
-set :user, 'root'
+#server details
+default_run_options[:pty] = true  # Must be set for the password prompt
+set :deploy_to, "/srv/www/railsapp/iktas.me"  #部署在服务器上的地址
+
+set :user, "root" #ssh连接服务器的帐号
 set :use_sudo, false
-set :deploy_to, '/home/ROR/kitas.me'
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+set :ssh_options, { :forward_agent => true }
+#repo details
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm_username, ENV["GITHUB_USERNAME"] #github帐号
+set :scm_passphrase, ENV["GITHUB_PASSWORD"] #设置github  ssh时设置到密码
+set :repository,  "git@github.com:dreamlx/iktas.me.git" #项目在github上的帐号
+set :branch, "master" #github上具体的分支
+set :deploy_via, :remote_cache
 
-# Database
-set :db_password do
-  Capistrano::CLI.password_prompt("database password:")
-end
-
-# RVM
-require "rvm/capistrano"
-set :rvm_type, :user
-
-# Bundler
-require "bundler/capistrano"
-
-# Whenever
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
-
+#tasks
 namespace :deploy do
-  # Setup database
-  namespace :db do
-    desc "Generate database configuration"
-    task :setup do
-      run "mkdir -p #{File.join(shared_path, 'config')}"
-      db_config = <<-EOF
-        adapter: mysql2
-        encoding: utf8
-        reconnect: false
-        database: berlin_production
-        pool: 5
-        username: railsadmin
-        password: #{db_password}
-        socket: /var/run/mysqld/mysqld.sock
-      EOF
-      put db_config, File.join(shared_path, 'config', 'database.yml')
-    end
+desc "SCP transfer figaro configuration to the shared folder"
 
-    desc "[internal] Updates the symlink for database.yml file to the just deployed release."
-    task :create_symlink, :except => { :no_release => true } do
-      run "ln -nfs #{File.join(shared_path, 'config', 'database.yml')} #{File.join(release_path, 'config', 'database.yml')}"
-    end
-  end
-  after "deploy:setup", "deploy:db:setup" unless fetch(:skip_db_setup, false)
-  after "deploy:create_symlink", "deploy:db:create_symlink"
-  after "deploy:update", "deploy:migrate"
 
-  # Restart Passenger
-  task :start do ; end
-  task :stop do ; end
-  task :restart, roles: :app, except: { no_release: true } do
-    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
-  end
-
-  task :warm_up do
-    run "curl --silent #{url} > /dev/null" if fetch(:url, false)
-  end
-  after "deploy:restart", "deploy:warm_up"
-
-  namespace :carrierwave do
-    desc "Create symlink"
-    task :create_symlink, :except => { :no_release => true } do
-      run "mkdir -p #{File.join(shared_path, 'uploads')}"
-      run "ln -nfs #{File.join(shared_path, 'uploads')} #{File.join(release_path, 'public', 'uploads')}"
-    end
-    after "deploy:create_symlink", "deploy:carrierwave:create_symlink"
-  end
-
-  namespace :custom_configuration do
-    desc "Generate configuration"
-    task :setup do
-      run "mkdir -p #{File.join(shared_path, 'config')}"
-      run "touch #{File.join(shared_path, 'config', 'application.yml')}"
-    end
-
-    desc "Create symlink"
-    task :create_symlink, :except => { :no_release => true } do
-      #run "rm #{File.join(current_path, 'config', 'application.yml')}"
-      run "ln -nfs #{File.join(shared_path, 'config', 'application.yml')} #{File.join(release_path, 'config', 'application.yml')}"
-    end
-    after "deploy:setup", "deploy:custom_configuration:setup" unless fetch(:skip_db_setup, false)
-    after "deploy:create_symlink", "deploy:custom_configuration:create_symlink"
-  end
+task :restart, :roles => :app do
+  run "touch #{current_path}/tmp/restart.txt"
+  
 end
+
+task :stop, :roles => :app do
+    #do nonthing
+end
+
+desc "Symlink shared resources on each release - not used"
+task :symlink_shared, :roles => :app do
+  
+end
+
+task :precompile, :roles => :web do  
+    run "cd #{current_path} && #{rake} RAILS_ENV=production assets:precompile"  
+end  
+
+end
+
+after "deploy:update", "deploy:symlink_shared" 
+after "deploy:update", "deploy:migrate"
+# after "deploy:migrate", "deploy:precompile"
